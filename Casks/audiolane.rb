27afc78ac4be2d4294c1a44a697becd626d7b489
Kpodo Winner Junior <87322420+winner14@users.noreply.github.com>
2026-03-15 18:08:25 +0000

    Update Audiolane cask with new version and URL

--- Casks/audiolane.rb
+++ Casks/audiolane.rb
@@ -1,8 +1,8 @@
 cask "audiolane" do
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "4af3ded0d1507652255e409582d44233d39305960691f55596123c72c07d6e95"
 
-  url "https://github.com/winner14/audiolane/releases/tag/v#{version}/AudioLane.dmg"
+  url "https://github.com/winner14/audiolane/releases/download/v#{version}/AudioLane_final.zip"
   name "AudioLane"
   desc "Per-app audio routing for macOS"
   homepage "https://github.com/winner14/audiolane"
